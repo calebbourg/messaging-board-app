4b3bd50adde4da4698425f4fd4cce7f409c7bcb7
caleb bourg <calebbourg2@gmail.com>
2017-04-20 17:45:59 -0700

    override after_sign_up_path

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,9 +2,9 @@ class Users::SessionsController < Devise::SessionsController
 # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource/sign_in
   def create
@@ -16,7 +16,11 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def after_sign_in_path_for(resource)
+    root_url
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
